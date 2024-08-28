b0851a73c7321f2470f7834acf61304135f9671f
Joyce <joycesantos8900@gmail.com>
2024-08-28 14:46:37 -0300

    ApiGithub

---
+++ script.js
@@ -0,0 +1,35 @@
+const div = window.document.getElementById("div")
+const buscar = () => {
+  div.innerHTML = ""
+  let nome = document.getElementById("input").value.trim()
+  // const userName = "Joyce8900"
+  fetch(`https://api.github.com/users/${nome}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/vnd.github.v3+json",
+    },
+  }).then((response) => {
+    console.log(typeof response)
+    console.log(response)
+    return response.json()
+  }).then((data) => {
+    let img = document.createElement("img")
+    img.src = `${data.avatar_url}`
+    img.style.width = "150px"
+    img.style.height = "150px"
+    img.style.borderRadius = "50%"
+    img.style.border = "2px solid #000" 
+    img.style.objectFit = "cover"
+    div.appendChild(img)
+    let h2 = document.createElement("h2")
+    h2.innerHTML = `${data.name}`
+    div.appendChild(h2)
+    let p = document.createElement("p")
+    p.innerHTML = `Id: ${data.id}<br> Bio: ${data.bio}<br>Compania: ${data.company}<br> Local: ${data.location}`
+
+    div.appendChild(p)
+    console.log(data)
+  }).catch((err) => {
+    console.log("Erro: " + err)
+  })
+}
